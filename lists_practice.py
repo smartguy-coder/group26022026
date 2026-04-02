c76f6168595ea476bcf0121ee5d2000cfdcb3f11
vasylkartychak <w.i.k.mailua@gmail.com>
2026-04-02 20:02:43 +0300

    practice

---
+++ lists_practice.py
@@ -0,0 +1,31 @@
+my_hobbies = (
+    "soccer tennis chess         kdghku e guegyery guoe"
+    "rh guieriugi yergreg yertuygoiuerygiouryiougyrte"
+    "iouygiu  rtgyrtiougyrtyirtyrtitrryiourtitrgti ut"
+)
+print(my_hobbies)
+something_new = my_hobbies.split()
+print(something_new)
+my_list0a = []
+my_list0b = ['apples', 'milk', 'bread']
+my_list0c = [
+    'apples',  # macintosh
+    'milk',
+    'bread',
+    'milk',  # selyanske
+    'bread',
+    'milk',
+    'bread',
+    'milk',
+    'bread',
+    'bread',
+    'bread',
+    'milk',
+    "#' ,",
+    "data",
+]
+print(my_list0c)
+my_list1 = ['soccer', 'tennis', 'chess']
+print("#")
+
+
