d61b466ce6bc2450e27e451b215c1f1f6d9eedaf
vasylkartychak <w.i.k.mailua@gmail.com>
2026-05-25 21:19:52 +0300

    oop more practice

--- models.py
+++ models.py
@@ -1,8 +1,36 @@
 
+from typing import Self
 
 
-class Person:
+class FinancialCalculatorMixin:
+    def __init__(self, accounts: list["BankAccount"] = None):
+        self.accounts: list[BankAccount] = []
+        if accounts:
+            self.accounts: list[BankAccount] = accounts
+
+    @property
+    def money(self) -> int:
+        # my_money = 0
+        # for account in self.accounts:
+        #     my_money += account.balance
+
+        summs = [account.balance for account in self.accounts]
+        print(summs)
+        return sum(summs)
+
+    def __eq__(self, other: Self):
+        return self.money == other.money
+
+    def __ge__(self, other: Self):
+        return self.money >= self.money
+
+    def __gt__(self, other: Self):
+        return self.money > other.money
+
+
+class Person(FinancialCalculatorMixin):
     def __init__(self, name: str):
+        super().__init__()
         self.name = name.strip().title()
         print('created ', self)
 
@@ -17,18 +45,32 @@ class BankAccount:
         self.bank = bank
         print(self)
 
+    def deposit(self, amount: int):
+        self.balance += amount
+
+    def withdraw(self, amount: int):
+        self.balance -= amount
+
+    def transfer_money(self, other: Self, amount: int):
+        self.balance -= amount
+        other.balance += amount
+        print(f'Money transferred {amount} ({self} -> {other})')
+
+
     def __str__(self):
         return f"Account was opened in {self.bank} for {self.owner.name}"
 
 
-class Bank:
+class Bank(FinancialCalculatorMixin):
     def __init__(self, title: str):
+        super().__init__()
         self.title = f'LTD {title.strip().upper()}'
         print(self)
 
     def open_account(self, client: Person) -> BankAccount:
         bank_account = BankAccount(owner=client, bank=self)
-
+        self.accounts.append(bank_account)
+        client.accounts.append(bank_account)
         return bank_account
 
     def __str__(self):
