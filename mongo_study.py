9dbc7e375631f8d579dde7c762fc19ee65e8dd9f
vasylkartychak <w.i.k.mailua@gmail.com>
2026-06-04 19:44:42 +0300

    mongo temp

--- mongo_study.py
+++ mongo_study.py
@@ -12,12 +12,13 @@ uri = f"mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}@cluster0.faw86h6.mongodb
 
 client = MongoClient(uri, server_api=ServerApi('1'))
 
-databases = client.list_databases()
-print(databases)
-for db in databases:
-    print(db)
+# databases = client.list_databases()
+# print(databases)
+# for db in databases:
+#     print(db)
 
-db_shop = client.shop
+# db_shop = client.shop
+db_shop = client['outlet']
 
 collection_books = db_shop.books
 collection_phones = db_shop['phones']
@@ -26,6 +27,28 @@ collection_phones = db_shop['phones']
 # add one document
 book = {'title': '10 negro', "price": 365, 'description': "English classic detective"}
 collection_books.insert_one(book)
+
+phone = {'title': 'iPhone 17', "price": 65000, 'description': "cool"}
+created_phone = collection_phones.insert_one(phone)
+print(created_phone)
+
+# add many
+phones = [
+    {'title': 'iPhone 17', "price": 65000, 'description': "cool"},
+    {'title': 'iPhone 16', "price": 65000, 'description': "cool"},
+    {'title': 'iPhone 15', "price": 65000, 'description': "cool"},
+    {'title': 'iPhone 14', "price": 65000, 'description': "cool", 'is_restored': True},
+]
+created_phones = collection_phones.insert_many(phones)
+print(created_phones)
+
+
+
+
+
+
+
+
 
 
 
