09fd8d5e8ae3bfe3cc696de8e62285ea5bfb01b6
vasylkartychak <w.i.k.mailua@gmail.com>
2026-06-04 19:31:10 +0300

    mongo temp

--- mongo_study.py
+++ mongo_study.py
@@ -12,3 +12,38 @@ uri = f"mongodb+srv://{MONGO_USERNAME}:{MONGO_PASSWORD}@cluster0.faw86h6.mongodb
 
 client = MongoClient(uri, server_api=ServerApi('1'))
 
+databases = client.list_databases()
+print(databases)
+for db in databases:
+    print(db)
+
+db_shop = client.shop
+
+collection_books = db_shop.books
+collection_phones = db_shop['phones']
+
+# CREATE
+# add one document
+book = {'title': '10 negro', "price": 365, 'description': "English classic detective"}
+collection_books.insert_one(book)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
